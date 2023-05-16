6afe6b8919cf5412c3893fca85afe100bf979cb2
Jaden Kim <jadenikim@gmail.com>
2023-05-16 15:37:21 -0700

    Removed right border when expanding logo description.

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,5 +1,6 @@
 const toggleDescription = (button, image, description) => {
   const topDiv = document.getElementById(button);
+  const subsectionDiv = topDiv.querySelector(".subsection");
   const nestedDiv = topDiv.querySelector(".subsection .dynamic-subheading .subcol");
   const a = document.getElementById(image);
   const b = document.getElementById(description);
@@ -7,10 +8,12 @@ const toggleDescription = (button, image, description) => {
   if (a.style.display === "none") {
     a.style.display = "block";
     b.style.display = "none";
+    subsectionDiv.style.borderRight = "1px solid black";
     nestedDiv.textContent = "Explain";
   } else {
     a.style.display = "none";
     b.style.display = "block";
+    subsectionDiv.style.borderRight = "none";
     nestedDiv.textContent = "View";
   }
 };
